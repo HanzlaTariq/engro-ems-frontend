e0675291940eddf43cb80aeedc0a5f46a455fa2b
Hanzla Tariq <thanzla2005@gmail.com>
2026-01-12 23:11:00 +0500

    comment 5

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Axios instance
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
